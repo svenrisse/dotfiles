13ba01bfc02a0c966d00d72de4ec66a3078455de
Sven <svenrisse0@gmail.com>
2023-02-22 00:57:55 +0100

    feat(nvim): new maps for barbar tabs

--- nvim/lua/sven/core/maps.lua
+++ nvim/lua/sven/core/maps.lua
@@ -27,9 +27,9 @@ keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & heig
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+keymap.set("n", "<leader>tx", "<Cmd>BufferClose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", "<Cmd>BufferNext<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", "<Cmd>BufferPrevious<CR>") --  go to previous tab
 
 ----------------------
 -- Plugin Keybinds

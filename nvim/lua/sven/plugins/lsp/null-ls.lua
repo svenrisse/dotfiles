4b30f00f47b30c9a67bc5e0c07a5236ace740872
Sven <svenrisse0@gmail.com>
2023-09-21 23:05:37 +0200

    add black formatting

--- nvim/lua/sven/plugins/lsp/null-ls.lua
+++ nvim/lua/sven/plugins/lsp/null-ls.lua
@@ -6,28 +6,29 @@ local code_actions = null_ls.builtins.code_actions
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-	sources = {
-		formatting.stylua,
-		formatting.prettierd.with({
-			extra_filetypes = { "astro" },
-		}),
-		formatting.prismaFmt,
-		diagnostics.eslint_d,
-		code_actions.eslint_d,
-		formatting.rustfmt,
-	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({
-						bufnr = bufnr,
-					})
-				end,
-			})
-		end
-	end,
+  sources = {
+    formatting.stylua,
+    formatting.prettierd.with({
+      extra_filetypes = { "astro" },
+    }),
+    formatting.prismaFmt,
+    formatting.black,
+    diagnostics.eslint_d,
+    code_actions.eslint_d,
+    formatting.rustfmt,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            bufnr = bufnr,
+          })
+        end,
+      })
+    end
+  end,
 })

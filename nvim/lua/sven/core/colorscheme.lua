8d6695fa9d91087110ef99408cfbfc7502635418
Sven <svenrisse0@gmail.com>
2023-09-21 23:05:15 +0200

    change colorscheme

--- nvim/lua/sven/core/colorscheme.lua
+++ nvim/lua/sven/core/colorscheme.lua
@@ -4,8 +4,8 @@
 -- colorscheme tokyonight-storm
 -- colorscheme onedark
 
-local status, _ = pcall(vim.cmd, "colorscheme onedark")
+local status, _ = pcall(vim.cmd, "colorscheme tokyonight-storm")
 if not status then
-	print("Colorscheme not found!") -- print error if colorscheme not installed
-	return
+  print("Colorscheme not found!") -- print error if colorscheme not installed
+  return
 end

a9295d810e5bd044bca324865de0d102e1a8a7fd
Sven <svenrisse0@gmail.com>
2023-12-18 12:11:31 +0100

    change to catppuccin

--- nvim/lua/sven/plugins/colorscheme.lua
+++ nvim/lua/sven/plugins/colorscheme.lua
@@ -1,7 +1,8 @@
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme onedark]])
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd([[colorscheme catppuccin-mocha]])
+	end,
 }

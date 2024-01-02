4d50b46b0354cf6cb6e3fefed491adb4610212e4
Sven <svenrisse0@gmail.com>
2024-01-02 22:50:56 +0100

    save on events

--- nvim/lua/sven/plugins/harpoon.lua
+++ nvim/lua/sven/plugins/harpoon.lua
@@ -4,7 +4,12 @@ return {
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			require("harpoon"):setup()
+			require("harpoon"):setup({
+				settings = {
+					save_on_toggle = true,
+					sync_on_ui_close = true,
+				},
+			})
 		end,
 		keys = {
 			{

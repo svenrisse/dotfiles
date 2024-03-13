e2556337b616d6688f50280776a6d70d5d9d030c
Sven <svenrisse0@gmail.com>
2024-03-14 00:56:06 +0100

    fix tab bug luasnip

--- .config/nvim/lua/sven/plugins/nvim-cmp.lua
+++ .config/nvim/lua/sven/plugins/nvim-cmp.lua
@@ -69,12 +69,8 @@ return {
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
-					elseif luasnip.expandable() then
-						luasnip.expand()
-					elseif luasnip.expand_or_jumpable() then
+					elseif luasnip.expand_or_locally_jumpable() then
 						luasnip.expand_or_jump()
-					elseif check_backspace() then
-						fallback()
 					else
 						fallback()
 					end

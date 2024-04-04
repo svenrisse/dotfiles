650998c9d64a30a55033ce600fc444f82657b171
Sven <svenrisse0@gmail.com>
2024-04-04 21:39:06 +0200

    add yankbank hotkey

--- .config/nvim/lua/sven/plugins/yankbank.lua
+++ .config/nvim/lua/sven/plugins/yankbank.lua
@@ -1,5 +1,11 @@
 return {
 	"ptdewey/yankbank-nvim",
+	init = function()
+		local wk = require("which-key")
+		wk.register({
+			["<leader>y"] = { ":YankBank<CR>", "YankBank" },
+		})
+	end,
 	config = function()
 		require("yankbank").setup()
 	end,

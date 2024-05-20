b419f03ba692d99618963b2f398eb2e940ecefd1
Sven <svenrisse0@gmail.com>
2024-05-20 09:16:12 +0200

    comment out ts_context_commentstring

--- .config/nvim/lua/sven/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/sven/plugins/nvim-treesitter.lua
@@ -57,7 +57,7 @@ return {
 			})
 
 			-- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-			require("ts_context_commentstring").setup({})
+			-- require("ts_context_commentstring").setup({})
 		end,
 	},
 }

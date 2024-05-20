c12fee2cf1f38055dcca32524c7455896d0ed590
Sven <svenrisse0@gmail.com>
2024-05-20 09:15:31 +0200

    update

--- .config/nvim/lua/sven/core/options.lua
+++ .config/nvim/lua/sven/core/options.lua
@@ -4,14 +4,16 @@ local yank_group = augroup("HighlightYank", {})
 local autocmd = vim.api.nvim_create_autocmd
 
 -- line numbers
-opt.relativenumber = false -- show relative line numbers
+opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- 2 spaces for indent width
+vim.opt.softtabstop = 4
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+vim.g.rust_recommended_style = false
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping

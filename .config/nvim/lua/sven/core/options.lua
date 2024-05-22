a4d783c780fb53bb3ece15389dbddbc7f4d5c6d4
Sven <svenrisse0@gmail.com>
2024-05-22 20:43:57 +0200

    add lsp_signature and reduce cmdheight

--- .config/nvim/lua/sven/core/options.lua
+++ .config/nvim/lua/sven/core/options.lua
@@ -35,13 +35,15 @@ vim.opt.updatetime = 50
 -- Displays which-key popup sooner
 vim.opt.timeoutlen = 300
 
--- appearance
+vim.lsp.buf.signature_help = false
 
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+vim.opt.cmdheight = 0
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position

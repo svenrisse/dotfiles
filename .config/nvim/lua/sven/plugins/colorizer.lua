c0c882dc1b933893713123b7eccc2998198f43db
Sven <svenrisse0@gmail.com>
2024-05-22 20:44:26 +0200

    nvchad/nvim-colorizer -> brenoprata10/nvim/highlight-colors

--- .config/nvim/lua/sven/plugins/colorizer.lua
+++ .config/nvim/lua/sven/plugins/colorizer.lua
@@ -1,32 +1,28 @@
 return {
-  "NvChad/nvim-colorizer.lua",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function () 
-    local colorizer = require("colorizer")
+	"brenoprata10/nvim-highlight-colors",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("nvim-highlight-colors").setup({
+			---Render style
+			---@usage 'background'|'foreground'|'virtual'
+			render = "foreground",
 
-    colorizer.setup({
-      filetypes = { "*" },
-      user_default_options = {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        names = true, -- "Name" codes like Blue or blue
-        RRGGBBAA = false, -- #RRGGBBAA hex codes
-        AARRGGBB = false, -- 0xAARRGGBB hex codes
-        rgb_fn = false, -- CSS rgb() and rgba() functions
-        hsl_fn = false, -- CSS hsl() and hsla() functions
-        css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        -- Available modes for `mode`: foreground, background,  virtualtext
-        mode = "foreground", -- Set the display mode.
-        -- Available methods are false / true / "normal" / "lsp" / "both"
-        -- True is same as normal
-        tailwind = "both", -- Enable tailwind colors
-        -- parsers can contain values used in |user_default_options|
-        sass = { enable = false, parsers = { css }, }, -- Enable sass colors
-        virtualtext = "■",
-      },
-      -- all the sub-options of filetypes apply to buftypes
-      buftypes = {},
-      })
-    end,
+			---Set virtual symbol (requires render to be set to 'virtual')
+			virtual_symbol = "■",
+
+			---Highlight named colors, e.g. 'green'
+			enable_named_colors = true,
+
+			---Highlight tailwind colors, e.g. 'bg-blue-500'
+			enable_tailwind = true,
+
+			---Set custom colors
+			---Label must be properly escaped with '%' to adhere to `string.gmatch`
+			--- :help string.gmatch
+			custom_colors = {
+				{ label = "%-%-theme%-primary%-color", color = "#0f1219" },
+				{ label = "%-%-theme%-secondary%-color", color = "#5a5d64" },
+			},
+		})
+	end,
 }

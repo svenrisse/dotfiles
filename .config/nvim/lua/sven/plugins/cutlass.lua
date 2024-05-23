02413c34b3205886d944b4610d2cd159307334ad
Sven <svenrisse0@gmail.com>
2024-05-23 08:34:50 +0200

    add cutlass

---
+++ .config/nvim/lua/sven/plugins/cutlass.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"gbprod/cutlass.nvim",
+		opts = {
+			cut_key = "x",
+			override_del = nil,
+			exclude = {},
+			registers = {
+				select = "_",
+				delete = "_",
+				change = "_",
+			},
+		},
+	},
+}

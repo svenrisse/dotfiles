c21b4edd1ab67436aa5ceb5c7564c416e4b5c3dc
Sven <svenrisse0@gmail.com>
2024-05-26 11:50:31 +0200

    lsp-signature, disable virtual hints

--- .config/nvim/lua/sven/plugins/lsp/lsp-signature.lua
+++ .config/nvim/lua/sven/plugins/lsp/lsp-signature.lua
@@ -1,7 +1,9 @@
 return {
 	"ray-x/lsp_signature.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		hint_enable = false,
+	},
 	config = function(_, opts)
 		require("lsp_signature").setup(opts)
 	end,

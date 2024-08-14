ce2b6e2538c3e3101d39810b0635e0374df47b1a
Sven Risse <svenrisse0@gmail.com>
2024-08-14 12:57:42 +0200

    update which-key bindings

--- .config/nvim/lua/sven/plugins/silicon.lua
+++ .config/nvim/lua/sven/plugins/silicon.lua
@@ -2,12 +2,6 @@ return {
 	"michaelrommel/nvim-silicon",
 	lazy = true,
 	cmd = "Silicon",
-	init = function()
-		local wk = require("which-key")
-		wk.register({
-			["<leader>sc"] = { ":Silicon<CR>", "Snapshot Code" },
-		}, { mode = "v" })
-	end,
 	config = function()
 		require("silicon").setup({
 			-- Configuration here, or leave empty to use defaults

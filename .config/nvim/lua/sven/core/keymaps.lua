1cc961686ec766b90b46a1abd920db7028f975a8
Sven Risse <sven@svenrisse.com>
2024-11-22 09:34:04 +0100

    add FAR

--- .config/nvim/lua/sven/core/keymaps.lua
+++ .config/nvim/lua/sven/core/keymaps.lua
@@ -1,5 +1,6 @@
 -- set leader key to space
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
@@ -47,6 +48,9 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+-- far
+keymap.set("n", "<leader>far", "<cmd>GrugFar<cr>") -- list available help tags
 
 -- telescope git commands
 -- keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]

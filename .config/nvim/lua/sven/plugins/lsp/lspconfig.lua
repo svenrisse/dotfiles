6c511bf6b0cb82343cf63fa70b69c339cbfd011c
Sven Risse <svenrisse0@gmail.com>
2024-11-28 12:05:11 +0100

    remove comment

--- .config/nvim/lua/sven/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/sven/plugins/lsp/lspconfig.lua
@@ -84,7 +84,6 @@ return {
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
 		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type

0a7c5e2bf5011077357399cfe1e1f3a380dcfc18
Sven Risse <svenrisse0@gmail.com>
2024-12-07 15:12:33 +0100

    neovim/plugin: enable git snacks

--- .config/nvim/lua/sven/plugins/snacks.lua
+++ .config/nvim/lua/sven/plugins/snacks.lua
@@ -3,8 +3,69 @@ return {
 	priority = 1000,
 	lazy = false,
 	opts = {
+		bigfile = { enabled = true },
+		-- bufdelete = { enabled = true },
+		dashboard = { enabled = true },
+		-- debug = { enabled = true },
+		git = { enabled = true },
+		gitbrowse = { enabled = true },
+		lazygit = { enable = true },
+		-- notify = { enabled = true },
+		-- notifier = {
+		-- 	enabled = true,
+		-- 	timeout = 3000,
+		-- },
+		-- profiler = { enabled = true },
+		quickfile = { enabled = true },
+		-- rename = { enabled = true },
+		-- scratch = { enabled = true },
+		-- statuscolumn = { enabled = true },
+		-- terminal = { enabled = true },
 		toggle = { enabled = true },
+		-- win = { enabled = true },
+		-- words = { enabled = true },
+		-- styles = {
+		-- 	notification = {
+		-- 		wo = { wrap = true }, -- Wrap notifications
+		-- 	},
+		-- },
 	},
+    -- stylua: ignore
+    keys = {
+        -- { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+        -- { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+        -- { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+        -- { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+        -- { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+        { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
+        { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+        { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+        { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+        { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+        -- { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+        -- { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+        -- { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+        -- { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+        -- { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+    {
+      "<leader>N",
+      desc = "Neovim News",
+      function()
+        Snacks.win({
+          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+          width = 0.6,
+          height = 0.6,
+          wo = {
+            spell = false,
+            wrap = false,
+            signcolumn = "yes",
+            statuscolumn = " ",
+            conceallevel = 3,
+          },
+        })
+      end,
+    }
+  },
 	init = function()
 		vim.api.nvim_create_autocmd("User", {
 			pattern = "VeryLazy",

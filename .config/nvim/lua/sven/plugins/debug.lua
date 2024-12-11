1089a5a3faba6ed46c025b07a2d34586427d5d7c
Sven Risse <svenrisse0@gmail.com>
2024-12-11 08:25:25 +0100

    neovim/plugin: working debugging for go

--- .config/nvim/lua/sven/plugins/debug.lua
+++ .config/nvim/lua/sven/plugins/debug.lua
@@ -21,26 +21,24 @@ return {
 	{
 		"mfussenegger/nvim-dap",
 		recommended = true,
-		desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
 
 		dependencies = {
 			"rcarriga/nvim-dap-ui",
-			-- virtual text for the debugger
-			{
-				"theHamsta/nvim-dap-virtual-text",
-				opts = {},
-			},
+			"leoluz/nvim-dap-go",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
+			"williomboman/mason.nvim",
 		},
 
 		keys = {
 			{ "<leader>d", "", desc = "+debug", mode = { "n", "v" } },
-			{
-				"<leader>dB",
-				function()
-					require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-				end,
-				desc = "Breakpoint Condition",
-			},
+			-- {
+			-- 	"<leader>dB",
+			-- 	function()
+			-- 		require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+			-- 	end,
+			-- 	desc = "Breakpoint Condition",
+			-- },
 			{
 				"<leader>db",
 				function()
@@ -55,13 +53,13 @@ return {
 				end,
 				desc = "Run/Continue",
 			},
-			{
-				"<leader>da",
-				function()
-					require("dap").continue({ before = get_args })
-				end,
-				desc = "Run with Args",
-			},
+			-- {
+			-- 	"<leader>da",
+			-- 	function()
+			-- 		require("dap").continue({ before = get_args })
+			-- 	end,
+			-- 	desc = "Run with Args",
+			-- },
 			{
 				"<leader>dC",
 				function()
@@ -69,76 +67,76 @@ return {
 				end,
 				desc = "Run to Cursor",
 			},
-			{
-				"<leader>dg",
-				function()
-					require("dap").goto_()
-				end,
-				desc = "Go to Line (No Execute)",
-			},
-			{
-				"<leader>di",
-				function()
-					require("dap").step_into()
-				end,
-				desc = "Step Into",
-			},
-			{
-				"<leader>dj",
-				function()
-					require("dap").down()
-				end,
-				desc = "Down",
-			},
-			{
-				"<leader>dk",
-				function()
-					require("dap").up()
-				end,
-				desc = "Up",
-			},
-			{
-				"<leader>dl",
-				function()
-					require("dap").run_last()
-				end,
-				desc = "Run Last",
-			},
-			{
-				"<leader>do",
-				function()
-					require("dap").step_out()
-				end,
-				desc = "Step Out",
-			},
-			{
-				"<leader>dO",
-				function()
-					require("dap").step_over()
-				end,
-				desc = "Step Over",
-			},
-			{
-				"<leader>dp",
-				function()
-					require("dap").pause()
-				end,
-				desc = "Pause",
-			},
-			{
-				"<leader>dr",
-				function()
-					require("dap").repl.toggle()
-				end,
-				desc = "Toggle REPL",
-			},
-			{
-				"<leader>ds",
-				function()
-					require("dap").session()
-				end,
-				desc = "Session",
-			},
+			-- {
+			-- 	"<leader>dg",
+			-- 	function()
+			-- 		require("dap").goto_()
+			-- 	end,
+			-- 	desc = "Go to Line (No Execute)",
+			-- },
+			-- {
+			-- 	"<leader>di",
+			-- 	function()
+			-- 		require("dap").step_into()
+			-- 	end,
+			-- 	desc = "Step Into",
+			-- },
+			-- {
+			-- 	"<leader>dj",
+			-- 	function()
+			-- 		require("dap").down()
+			-- 	end,
+			-- 	desc = "Down",
+			-- },
+			-- {
+			-- 	"<leader>dk",
+			-- 	function()
+			-- 		require("dap").up()
+			-- 	end,
+			-- 	desc = "Up",
+			-- },
+			-- {
+			-- 	"<leader>dl",
+			-- 	function()
+			-- 		require("dap").run_last()
+			-- 	end,
+			-- 	desc = "Run Last",
+			-- },
+			-- {
+			-- 	"<leader>do",
+			-- 	function()
+			-- 		require("dap").step_out()
+			-- 	end,
+			-- 	desc = "Step Out",
+			-- },
+			-- {
+			-- 	"<leader>dO",
+			-- 	function()
+			-- 		require("dap").step_over()
+			-- 	end,
+			-- 	desc = "Step Over",
+			-- },
+			-- {
+			-- 	"<leader>dp",
+			-- 	function()
+			-- 		require("dap").pause()
+			-- 	end,
+			-- 	desc = "Pause",
+			-- },
+			-- {
+			-- 	"<leader>dr",
+			-- 	function()
+			-- 		require("dap").repl.toggle()
+			-- 	end,
+			-- 	desc = "Toggle REPL",
+			-- },
+			-- {
+			-- 	"<leader>ds",
+			-- 	function()
+			-- 		require("dap").session()
+			-- 	end,
+			-- 	desc = "Session",
+			-- },
 			{
 				"<leader>dt",
 				function()
@@ -146,116 +144,50 @@ return {
 				end,
 				desc = "Terminate",
 			},
-			{
-				"<leader>dw",
-				function()
-					require("dap.ui.widgets").hover()
-				end,
-				desc = "Widgets",
-			},
+			-- {
+			-- 	"<leader>dw",
+			-- 	function()
+			-- 		require("dap.ui.widgets").hover()
+			-- 	end,
+			-- 	desc = "Widgets",
+			-- },
 		},
 
 		config = function()
 			local dap = require("dap")
-			local icons = {
-				dap = {
-					Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
-					Breakpoint = " ",
-					BreakpointCondition = " ",
-					BreakpointRejected = { " ", "DiagnosticError" },
-					LogPoint = ".>",
-				},
-			}
+			local ui = require("dapui")
 
-			-- vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-			--
-			-- for name, sign in pairs(icons.dap) do
-			-- 	sign = type(sign) == "table" and sign or { sign }
-			-- 	vim.fn.sign_define(
-			-- 		"Dap" .. name,
-			-- 		{ text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
-			-- 	)
-			-- end
+			require("dapui").setup()
+			require("dap-go").setup()
 
-			dap.adapters["pwa-node"] = {
-				type = "server",
-				host = "localhost",
-				port = "${port}", --let both ports be the same for now...
-				executable = {
-					command = "node",
-					-- -- 💀 Make sure to update this path to point to your installation
-					args = {
-						vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
-						"${port}",
-					},
-					-- command = "js-debug-adapter",
-					-- args = { "${port}" },
-				},
-			}
+			require("nvim-dap-virtual-text").setup({
+				-- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+				display_callback = function(variable)
+					local name = string.lower(variable.name)
+					local value = string.lower(variable.value)
+					if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
+						return "*****"
+					end
 
-			for _, language in ipairs({ "typescript", "javascript" }) do
-				dap.configurations[language] = {
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch Current File (pwa-node)",
-						cwd = "${workspaceFolder}", -- vim.fn.getcwd(),
-						args = { "${file}" },
-						sourceMaps = true,
-						protocol = "inspector",
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch Current File (Typescript)",
-						cwd = "${workspaceFolder}",
-						runtimeArgs = { "--loader=ts-node/esm" },
-						program = "${file}",
-						runtimeExecutable = "node",
-						args = { "${file}" },
-						sourceMaps = true,
-						protocol = "inspector",
-						outFiles = { "${workspaceFolder}/**/**/*", "!**/node_modules/**" },
-						skipFiles = { "<node_internals>/**", "node_modules/**" },
-						resolveSourceMapLocations = {
-							"${workspaceFolder}/**",
-							"!**/node_modules/**",
-						},
-					},
-				}
+					if #variable.value > 15 then
+						return " " .. string.sub(variable.value, 1, 15) .. "... "
+					end
+
+					return " " .. variable.value
+				end,
+			})
+
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
 			end
-
-			-- setup dap config by VsCode launch.json file
-			local vscode = require("dap.ext.vscode")
-			local json = require("plenary.json")
-			vscode.json_decode = function(str)
-				return vim.json.decode(json.json_strip_comments(str))
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
 			end
-		end,
-	},
-
-	-- fancy UI for the debugger
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = { "nvim-neotest/nvim-nio" },
-    -- stylua: ignore
-    keys = {
-      { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
-      { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
-    },
-		opts = {},
-		config = function(_, opts)
-			local dap = require("dap")
-			local dapui = require("dapui")
-			dapui.setup(opts)
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open({})
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
 			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close({})
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close({})
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
 			end
 		end,
 	},

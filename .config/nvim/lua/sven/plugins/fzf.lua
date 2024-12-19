2327da2476fec2b4ab124821fad7162507d83d63
Sven Risse <svenrisse0@gmail.com>
2024-12-19 18:39:51 +0100

    neovim/plugin: add useful opts to fzf

--- .config/nvim/lua/sven/plugins/fzf.lua
+++ .config/nvim/lua/sven/plugins/fzf.lua
@@ -35,6 +35,14 @@ return {
 					["ctrl-h"] = { actions.toggle_hidden },
 				},
 			},
+			oldfiles = {
+				include_current_session = true,
+			},
+			previewers = {
+				builtin = {
+					syntax_limit_b = 1024 * 100, -- 100KB
+				},
+			},
 		})
 	end,
     -- stylua: ignore

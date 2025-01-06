189856d913b1dc6ad9f9545220a5a3d6c0d1db1e
Sven Risse <svenrisse0@gmail.com>
2025-01-06 10:31:18 +0100

    neovim/plugin: add colorful-menu for nicer cmp

--- .config/nvim/lua/sven/plugins/nvim-cmp.lua
+++ .config/nvim/lua/sven/plugins/nvim-cmp.lua
@@ -14,8 +14,6 @@ return {
 
 		local luasnip = require("luasnip")
 
-		local lspkind = require("lspkind")
-
 		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -94,6 +92,15 @@ return {
 			formatting = {
 				fields = { "kind", "abbr", "menu" },
 				format = function(entry, vim_item)
+					local highlights_info = require("colorful-menu").cmp_highlights(entry)
+
+					-- if highlight_info==nil, which means missing ts parser, let's fallback to use default `vim_item.abbr`.
+					-- What this plugin offers is two fields: `vim_item.abbr_hl_group` and `vim_item.abbr`.
+					if highlights_info ~= nil then
+						vim_item.abbr_hl_group = highlights_info.highlights
+						vim_item.abbr = highlights_info.text
+					end
+
 					vim_item.kind = kind_icons[vim_item.kind]
 
 					vim_item.menu = ({
@@ -104,6 +111,7 @@ return {
 						path = "",
 						emoji = "",
 					})[entry.source.name]
+
 					return vim_item
 				end,
 			},

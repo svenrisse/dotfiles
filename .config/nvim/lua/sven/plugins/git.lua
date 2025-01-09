ea95c81daf624e2712784a9e027ab9106c39bb4f
Sven Risse <svenrisse0@gmail.com>
2025-01-09 16:13:09 +0100

    neovim/plugins: add git-conflict keys and add bqf

--- .config/nvim/lua/sven/plugins/git.lua
+++ .config/nvim/lua/sven/plugins/git.lua
@@ -8,5 +8,15 @@ return {
 		"akinsho/git-conflict.nvim",
 		version = "*",
 		config = true,
+		keys = {
+			{ "<leader>gc", "", desc = "+git-conflict" },
+			{ "<leader>gcq", "<cmd>GitConflictListQf<cr>", desc = "Git Conflicts to quickfix" },
+			{ "<leader>gco", "<cmd>GitConflictChooseOurs<cr>", desc = "Choose ours / current" },
+			{ "<leader>gct", "<cmd>GitConflictChooseTheirs<cr>", desc = "Choose theirs / incoming" },
+			{ "<leader>gcb", "<cmd>GitConflictChooseBoth<cr>", desc = "Choose both" },
+			{ "<leader>gcx", "<cmd>GitConflictChooseNone<cr>", desc = "Choose none" },
+			{ "<leader>gcn", "<cmd>GitConflictNextConflict<cr>", desc = "Next Git Conflict" },
+			{ "<leader>gcp", "<cmd>GitConflictPrevConflict<cr>", desc = "Prev Git Conflict" },
+		},
 	},
 }

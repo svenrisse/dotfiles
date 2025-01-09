8b26656da11cc826e595ced913ed4e194889c4c9
Sven Risse <svenrisse0@gmail.com>
2025-01-09 10:22:05 +0100

    neovim/plugins: remove codelens and better todo hotkey

--- .config/nvim/lua/sven/plugins/todo-comments.lua
+++ .config/nvim/lua/sven/plugins/todo-comments.lua
@@ -6,6 +6,6 @@ return {
 		highlight = { pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]] },
 	},
 	keys = {
-		{ "<leader>fT", "<cmd>TodoFzfLua keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+		{ "<leader>ft", "<cmd>TodoFzfLua keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
 	},
 }

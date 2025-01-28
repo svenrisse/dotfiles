640473553b3c38c4858f85221367ccbe115d5db8
Sven Risse <svenrisse0@gmail.com>
2025-01-28 12:03:47 +0100

    neovim(gitsigns): show blame

--- .config/nvim/lua/sven/plugins/git.lua
+++ .config/nvim/lua/sven/plugins/git.lua
@@ -2,7 +2,17 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
-		config = true,
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame = true,
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 100,
+					virt_text_priority = 100,
+				},
+			})
+		end,
 	},
 	{
 		"akinsho/git-conflict.nvim",

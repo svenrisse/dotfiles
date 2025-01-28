416116772531e6d69bf6e44de05253ffe7eec932
Sven Risse <svenrisse0@gmail.com>
2025-01-28 11:31:46 +0100

    neovim(tiny-inline-diagnostics): cleanup

--- .config/nvim/lua/sven/plugins/tiny-inline-diagnostic.lua
+++ .config/nvim/lua/sven/plugins/tiny-inline-diagnostic.lua
@@ -1,28 +1,18 @@
 return {
 	"rachartier/tiny-inline-diagnostic.nvim",
-	event = "VeryLazy", -- Or `LspAttach`
-	priority = 1000, -- needs to be loaded in first
+	event = "VeryLazy",
+	priority = 1000,
 	config = function()
 		require("tiny-inline-diagnostic").setup({
 			options = {
-				-- Show the source of the diagnostic.
 				show_source = true,
-
-				-- If multiple diagnostics are under the cursor, display all of them.
 				multiple_diag_under_cursor = true,
-
-				-- Enable diagnostic message on all lines.
 				multilines = {
 					enabled = true,
 					always_show = true,
 				},
-
-				-- Show all diagnostics on the cursor line.
 				show_all_diags_on_cursorline = true,
-
-				-- Enable diagnostics on Insert mode. You should also se the `throttle` option to 0, as some artefacts may appear.
 				enable_on_insert = false,
-
 				virt_texts = {
 					priority = 2048,
 				},

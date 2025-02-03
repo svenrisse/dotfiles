3150cf7deab14bd600a120e85b51672d88d401a8
Sven Risse <svenrisse0@gmail.com>
2025-02-03 07:28:58 +0100

    fix: move vts commands into typescript lang file

--- .config/nvim/lua/sven/plugins/languages/typescript.lua
+++ .config/nvim/lua/sven/plugins/languages/typescript.lua
@@ -19,19 +19,20 @@ return {
 			})
 		end,
 		keys = {
-			{ "<leader>x", "<cmd>TSC<cr>", desc = "Project wide diagnostics (typescript)" },
+			{ "<leader>x", "<cmd>TSC<cr>", desc = "Project wide diagnostics (tsc)" },
+			{ "<leader>io", "<Cmd>VtsExec organize_imports<CR>", desc = "Organize Import (vts)" },
+			{ "<leader>im", "<Cmd>VtsExec add_missing_imports<CR>", desc = "Add missing Imports (vts)" },
+			{ "<leader>ir", "<Cmd>VtsExec remove_unused_imports<CR>", desc = "Remove unused Imports (vts)" },
+			{ "<leader>is", "<Cmd>VtsExec sort_imports<CR>", desc = "Sort Imports (vts)" },
+			{ "<leader>Fa", "<Cmd>VtsExec fix_all<CR>", desc = "Fix all (vts)" },
+			{ "<leader>Fr", "<Cmd>VtsExec rename_file<CR>", desc = "Rename File (vts)" },
+			{ "<leader>Ff", "<Cmd>VtsExec file_references<CR>", desc = "File References (vts)" },
 		},
 	},
 	{
 		"folke/ts-comments.nvim",
 		opts = {},
 		event = "VeryLazy",
-		enabled = vim.fn.has("nvim-0.10.0") == 1,
-	},
-	{
-		"dmmulroy/ts-error-translator.nvim",
-		config = function()
-			require("ts-error-translator").setup()
-		end,
+		enabled = true,
 	},
 }

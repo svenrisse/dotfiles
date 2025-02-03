514864850e6cd4e1362108e2c90c7bd980a05b74
Sven Risse <svenrisse0@gmail.com>
2025-02-03 07:29:15 +0100

    style: remove redundant braces

--- .config/nvim/lua/sven/plugins/colorscheme.lua
+++ .config/nvim/lua/sven/plugins/colorscheme.lua
@@ -1,14 +1,12 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				transparent_background = false,
-			})
-			vim.cmd([[colorscheme catppuccin]])
-		end,
-	},
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha",
+			transparent_background = false,
+		})
+		vim.cmd([[colorscheme catppuccin]])
+	end,
 }

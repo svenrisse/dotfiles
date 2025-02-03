b2058fe05a0dfd99e61e51682bb633ef7fd35b02
Sven Risse <svenrisse0@gmail.com>
2025-02-03 07:32:47 +0100

    fix: cleanup nvim-autopairs file

--- .config/nvim/lua/sven/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/sven/plugins/nvim-autopairs.lua
@@ -11,17 +11,11 @@ return {
 				ts_config = {
 					lua = { "string" }, -- don't add pairs in lua string treesitter nodes
 					javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-					java = false, -- don't check treesitter on java
 				},
 			})
 
-			-- import nvim-autopairs completion functionality
 			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-			-- import nvim-cmp plugin (completions plugin)
 			local cmp = require("cmp")
-
-			-- make autopairs and completion work together
 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 		end,
 	},

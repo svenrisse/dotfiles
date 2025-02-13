c5c89402e2aa5906549c52824d7d7440ee416255
Sven Risse <svenrisse0@gmail.com>
2025-02-13 11:01:59 +0100

    neovim: use new setup for nvim-ts-autotag

--- .config/nvim/lua/sven/plugins/treesitter.lua
+++ .config/nvim/lua/sven/plugins/treesitter.lua
@@ -18,10 +18,6 @@ return {
 				},
 				-- enable indentation
 				indent = { enable = true },
-				-- enable autotagging (w/ nvim-ts-autotag plugin)
-				autotag = {
-					enable = true,
-				},
 				-- ensure these language parsers are installed
 				ensure_installed = {
 					"json",

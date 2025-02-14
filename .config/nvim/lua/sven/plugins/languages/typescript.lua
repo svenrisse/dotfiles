310d81e4d59391d1bb7ccf875e91edd8bc725373
Sven Risse <svenrisse0@gmail.com>
2025-02-14 09:49:21 +0100

    neovim: remove vtsls for now

--- .config/nvim/lua/sven/plugins/languages/typescript.lua
+++ .config/nvim/lua/sven/plugins/languages/typescript.lua
@@ -1,16 +1,5 @@
 return {
 	{
-		"yioneko/nvim-vtsls",
-		ft = {
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact",
-		},
-		enabled = true,
-		dependencies = { "nvim-lspconfig" },
-	},
-	{
 		"dmmulroy/tsc.nvim",
 		config = function()
 			require("tsc").setup({

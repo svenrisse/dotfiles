a8434da6fbf2040c10092e8ab577cc8b72bd2721
Sven Risse <svenrisse0@gmail.com>
2025-02-14 09:49:43 +0100

    neovim: fix todo-comments highlighting

--- .config/nvim/lua/sven/plugins/todo-comments.lua
+++ .config/nvim/lua/sven/plugins/todo-comments.lua
@@ -1,13 +1,12 @@
 return {
 	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		{
-			"<leader>ft",
-			function()
-				Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
-			end,
-			desc = "Todo/Fix/Fixme",
-		},
-	},
+	dependencies = "nvim-lua/plenary.nvim",
+	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+	config = function()
+		require("todo-comments").setup({
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		})
+	end,
 }

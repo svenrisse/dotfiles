4f16cf9506c4a07bf155561a64b4a9af8951bb1c
Sven Risse <svenrisse0@gmail.com>
2025-02-17 08:06:20 +0100

    fix(neovim): remove lazydev source from cmp

--- .config/nvim/lua/sven/plugins/cmp.lua
+++ .config/nvim/lua/sven/plugins/cmp.lua
@@ -40,14 +40,7 @@ return {
 		signature = { window = { border = "single" } },
 
 		sources = {
-			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-			providers = {
-				lazydev = {
-					name = "LazyDev",
-					module = "lazydev.integrations.blink",
-					score_offset = 100,
-				},
-			},
+			default = { "lsp", "path", "snippets", "buffer" },
 		},
 	},
 	opts_extend = { "sources.default" },

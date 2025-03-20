3b9b3023e62d2035161aa329bc6c65a5215e0c55
Sven Risse <svenrisse0@gmail.com>
2025-03-20 10:00:27 +0100

    replace lsp-signature with cmp-nvim-lsp-signature-help

--- .config/nvim/lua/sven/plugins/cmp.lua
+++ .config/nvim/lua/sven/plugins/cmp.lua
@@ -4,6 +4,7 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
+		"hrsh7th/cmp-nvim-lsp-signature-help",
 		"L3MON4D3/LuaSnip", -- snippet engine
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
@@ -117,6 +118,7 @@ return {
 			},
 			sources = {
 				{ name = "nvim_lsp" },
+				{ name = "nvim_lsp_signature_help" },
 				{ name = "luasnip" },
 				{ name = "nvim_lua" },
 				{ name = "path" },
